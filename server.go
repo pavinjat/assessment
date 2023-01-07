3ca699ee4a42b8bd828ad7eee2988dba9c429c21
pavin jatura <pavin.jat@gmail.com>
2023-01-07 10:10:26 +0700

    go mod tidy

--- server.go
+++ server.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	fmt.Println("Please use server.go for main file")
 	fmt.Println("start at port:", os.Getenv("PORT"))
+
+	e := echo.New()
+
+	e.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		if username == "apidesign" || password == "45678" {
+			return true, nil
+		}
+		return false, nil
+	}))
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	log.Fatal(e.Start(":2565"))
 }
